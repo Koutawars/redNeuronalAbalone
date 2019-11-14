e73e5f71db89981489d3399766d7b3c958394c7a
Koutawars <admikouta@gmail.com>
2019-11-14 10:36:18 -0500

    XD

--- test.py
+++ test.py
@@ -2,7 +2,18 @@ import pandas as pd
 import numpy as np
 
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelEncoder
+from sklearn.neural_network import MLPRegressor
+
+
 
 data = pd.read_csv('abalone.csv')
+# cambiar de texto a datos
+data['Sex'] = LabelEncoder().fit_transform(data['Sex'].tolist())
+# calcular la edad con los anillos
+data['Age'] = data['Rings'] + 1.5
+# quitar los anillos
+data.drop('Rings', axis = 1, inplace = True)
 
-print(data)
+
+print(data.head())
