1fb27322772921abaf3b8d828a4eba3805d05893
Koutawars <admikouta@gmail.com>
2019-11-14 10:40:07 -0500

    XD

--- test.py
+++ test.py
@@ -1,11 +1,12 @@
+# Ejemplo de MLPregressor:
+# https://www.programcreek.com/python/example/93778/sklearn.neural_network.MLPRegressor
+# https://www.kaggle.com/ragnisah/eda-abalone-age-prediction 
 import pandas as pd
 import numpy as np
 
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import LabelEncoder
 from sklearn.neural_network import MLPRegressor
-
-
 
 data = pd.read_csv('abalone.csv')
 # cambiar de texto a datos
